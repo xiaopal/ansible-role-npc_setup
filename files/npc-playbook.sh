ad3b77d2c09ee4d00b7b587897439958f7fad2cd
Chen Xiaohui <graych@gmail.com>
2017-09-01 13:23:08 +0800

    read playbook from stdin

--- files/npc-playbook.sh
+++ files/npc-playbook.sh
@@ -1,4 +1,21 @@
 #! /bin/bash
-export NPC_SETUP_LOG="$(mktemp -u)" && mkfifo "$NPC_SETUP_LOG" && trap "rm -f '$NPC_SETUP_LOG'" EXIT && {
-	cat "$NPC_SETUP_LOG" >&2 & ( exec 100>$NPC_SETUP_LOG && ansible-playbook "$@" )
+
+do_playbook(){
+	local NPC_SETUP_TMP="$(mktemp -d)" && trap "rm -fr '$NPC_SETUP_TMP'" EXIT
+	local ARGS=() ARG
+	while ARG="$1" && shift; do
+		case "$ARG" in
+		--stdin|-)
+			local PLAYBOOK="$NPC_SETUP_TMP/stdin.yml"
+			cat>"$PLAYBOOK" && ARGS=("${ARGS[@]}" "$PLAYBOOK")
+			;;
+		*)
+			ARGS=("${ARGS[@]}" "$ARG")
+			;;
+		esac
+	done
+	export NPC_SETUP_LOG="$NPC_SETUP_TMP/setup.log" && mkfifo "$NPC_SETUP_LOG" && {
+		cat "$NPC_SETUP_LOG" >&2 & ( exec 100>$NPC_SETUP_LOG && ansible-playbook "${ARGS[@]}" )
+	}
 }
+do_playbook "$@"

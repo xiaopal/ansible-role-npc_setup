205722615fdd34ee0db265c7f01f363db662fd7b
Chen Xiaohui <graych@gmail.com>
2017-09-01 16:06:49 +0800

    -

--- files/npc-playbook.sh
+++ files/npc-playbook.sh
@@ -1,19 +1,37 @@
 #! /bin/bash
 
+SETUP_PLAYBOOK_YAML='---
+- hosts: localhost
+  gather_facts: no
+  roles: 
+    - xiaopal.npc_setup
+  pre_tasks:
+    - include_vars: setup-vars.yml'
+
 do_playbook(){
 	local NPC_SETUP_TMP="$(mktemp -d)" && trap "rm -fr '$NPC_SETUP_TMP'" EXIT
+	local STDIN="$NPC_SETUP_TMP/stdin.yml" \
+		SETUP_VARS="$NPC_SETUP_TMP/setup-vars.yml" \
+		SETUP_PLAYBOOK="$NPC_SETUP_TMP/setup.yml"
 	local ARGS=() ARG
 	while ARG="$1" && shift; do
 		case "$ARG" in
 		--stdin|-)
-			local PLAYBOOK="$NPC_SETUP_TMP/stdin.yml"
-			cat>"$PLAYBOOK" && ARGS=("${ARGS[@]}" "$PLAYBOOK")
+			cat>"$STDIN" && ARGS=("${ARGS[@]}" "$STDIN") || return 1
+			;;
+		--setup)
+			local VARS="${1:--}" && shift
+			[ "$VARS" = '-' ] && cat>"$SETUP_VARS" || {
+				[ -f "$VARS" ] && cat "$VARS">"$SETUP_VARS" || return 1
+			}
+			echo "$SETUP_PLAYBOOK_YAML">"$SETUP_PLAYBOOK" && ARGS=("${ARGS[@]}" "$SETUP_PLAYBOOK")
 			;;
 		*)
 			ARGS=("${ARGS[@]}" "$ARG")
 			;;
 		esac
 	done
+
 	export NPC_SETUP_LOG="$NPC_SETUP_TMP/setup.log" && mkfifo "$NPC_SETUP_LOG" && {
 		cat "$NPC_SETUP_LOG" >&2 & ( exec 100>$NPC_SETUP_LOG && ansible-playbook "${ARGS[@]}" )
 	}
